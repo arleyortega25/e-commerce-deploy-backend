fb6b3dc346de320a57ed96fa4cae95295a6332ab
arleyortega25 <oarleyfernando21@gmail.com>
2025-08-11 21:48:22 -0500

    fixing cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://e-commerce-deploy-frontend.vercel.app/',
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
